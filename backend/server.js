156609b31bd07e05d1ac4cca074bcbf20a4aefcd
CampusBites2026 <chat19675@gmail.com>
2026-07-13 12:01:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5500",
- 
+  "https://campusbitesfoodcourt-rho.vercel.app",
   "https://campusbitefoodcourt.vercel.app",         // ← your actual frontend URL
   "https://campusbitefoodcourtadmin.vercel.app",// ← update your admin URL too if needed
   "https://food-court-seln.onrender.com",
